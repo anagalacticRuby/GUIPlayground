54077d8c7d8b812f48b2bd4dc16df1592ae6dcbc
Nicholas Hansen <nchansen7678@eagle.fgcu.edu>
2019-08-28 17:55:59 -0400

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    void showName(MouseEvent event) {
+    String name;
+    
+    }
+
+}
